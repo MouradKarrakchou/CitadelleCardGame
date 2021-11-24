9223c914206dd473cd4304b435f754ccadf48428
AyoubIMAMI <youbima17@gmail.com>
2021-11-24 13:27:03 +0100

    More PlayerTests

--- src/test/java/fr/unice/polytech/citadelle/PlayerTest.java
+++ src/test/java/fr/unice/polytech/citadelle/PlayerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import fr.unice.polytech.citadelle.game.Player;
 import fr.unice.polytech.citadelle.game.District;
-import fr.unice.polytech.citadelle.game.City;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,10 +15,10 @@ import java.util.ArrayList;
 public class PlayerTest {
     //Initialization
     Player player = new Player("robot1");
-    City city = new City();
 
     @BeforeEach
     void beforeEach(){
+        player.districtCards.clear();
         player.districtCards.add(new District("Haunted City", 2));
         player.districtCards.add(new District("Prison", 2));
         player.districtCards.add(new District("Manor", 3));
@@ -27,12 +26,9 @@ public class PlayerTest {
         player.districtCards.add(new District("Laboratory", 5));
         player.districtCards.add(new District("Observatory", 5));
         player.districtCards.add(new District("University", 6));
+
+        player.getCity().builtDistrict.clear();
     }
-
-   //TESTS TO DO :
-        //public ArrayList<District> districtWeCanBuild(ArrayList<District> districtCheapEnough)
-
-        //public void buildDistrict(District districtToBuild)
 
     @Test
     //With 2 golds
@@ -77,16 +73,77 @@ public class PlayerTest {
         assertEquals(districts, player.districtWeHaveEnoughMoneyToBuild());
     }
 
-    /*
+
     @Test
     void isAlreadyBuiltTest(){
-        city.builtDistrict.add(new District("Haunted City", 2));
-        city.builtDistrict.stream().forEach(dis -> System.out.println(dis.getName()));
+        player.getCity().builtDistrict.add(new District("Haunted City", 2));
         assertTrue(player.isAlreadyBuilt("Haunted City"));
         assertFalse(player.isAlreadyBuilt("Prison"));
 
     }
-    //To do : the case where there is no district in the city
-    */
+
+    @Test
+    void isAlreadyBuiltEmptyTest(){
+        assertFalse(player.isAlreadyBuilt("Prison"));
+    }
+
+    @Test
+    void districtWeCanBuildTest(){
+        player.golds = 4;
+
+        player.getCity().builtDistrict.add(new District("Haunted City", 2));
+        player.getCity().builtDistrict.add(new District("Manor", 3));
+
+        ArrayList<District> districtCheapEnough = new ArrayList<>();
+        districtCheapEnough.add(new District("Haunted City", 2));
+        districtCheapEnough.add(new District("Prison", 2));
+        districtCheapEnough.add(new District("Manor", 3));
+        districtCheapEnough.add(new District("Castle", 4));
+
+        ArrayList<District> districts = new ArrayList<>();
+        districts.add(new District("Prison", 2));
+        districts.add(new District("Castle", 4));
+
+        assertEquals(districts, player.districtWeCanBuild(districtCheapEnough));
+    }
+
+    @Test
+    void zeroDistrictWeCanBuildTest(){
+        player.golds = 4;
+
+        player.getCity().builtDistrict.add(new District("Haunted City", 2));
+        player.getCity().builtDistrict.add(new District("Manor", 3));
+
+        ArrayList<District> districtCheapEnough = new ArrayList<>();
+        districtCheapEnough.add(new District("Haunted City", 2));
+        districtCheapEnough.add(new District("Manor", 3));
+
+        ArrayList<District> districts = new ArrayList<>();
+
+        assertEquals(districts, player.districtWeCanBuild(districtCheapEnough));
+    }
+
+    @Test
+    void buildDistrictTest(){
+        player.golds = 4;
+
+        ArrayList<District> districts = new ArrayList<>();
+        districts.add(new District("Manor", 3));
+
+        ArrayList<District> districts1 = new ArrayList<>();
+        districts1.add(new District("Haunted City", 2));
+        districts1.add(new District("Prison", 2));
+        districts1.add(new District("Castle", 4));
+        districts1.add(new District("Laboratory", 5));
+        districts1.add(new District("Observatory", 5));
+        districts1.add(new District("University", 6));
+
+        player.buildDistrict(new District("Manor", 3));
+
+        assertEquals(districts, player.getCity().builtDistrict);
+        assertEquals(districts1, player.getDistrictCards());
+        assertEquals(1, player.getGolds());
+        assertEquals(3, player.getScore());
+    }
 
 }
