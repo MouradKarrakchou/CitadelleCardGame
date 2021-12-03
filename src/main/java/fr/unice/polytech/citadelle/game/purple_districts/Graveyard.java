1262ca3e105d7e04ccd98f6a04fd02af66f7acf8
AyoubIMAMI <youbima17@gmail.com>
2021-12-03 18:25:54 +0100

    Adding district spell: Dragon Gate patch #27

--- src/main/java/fr/unice/polytech/citadelle/game/purple_districts/Graveyard.java
+++ src/main/java/fr/unice/polytech/citadelle/game/purple_districts/Graveyard.java
@@ -1,9 +1,15 @@
 package fr.unice.polytech.citadelle.game.purple_districts;
 
+import fr.unice.polytech.citadelle.game.Player;
 import fr.unice.polytech.citadelle.game.PurpleDistrict;
 
 public class Graveyard  extends PurpleDistrict {
     public Graveyard(String name, int value, String color, String nameOfFamily) {
         super(name, value, color, nameOfFamily);
+    }
+
+    @Override
+    public void spell(Player player) {
+
     }
 }
