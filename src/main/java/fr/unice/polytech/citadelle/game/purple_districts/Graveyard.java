9b1e4a0016865848d1063e859b3dca094ce038b5
AyoubIMAMI <youbima17@gmail.com>
2021-12-05 19:06:11 +0100

    Adding district spell: University + Test

--- src/main/java/fr/unice/polytech/citadelle/game/purple_districts/Graveyard.java
+++ src/main/java/fr/unice/polytech/citadelle/game/purple_districts/Graveyard.java
@@ -1,8 +1,8 @@
 package fr.unice.polytech.citadelle.game.purple_districts;
 
-import fr.unice.polytech.citadelle.game.District;
+import fr.unice.polytech.citadelle.game.SpellDistrict;
 
-public class Graveyard extends District {
+public class Graveyard extends SpellDistrict {
 
     public Graveyard(String name, int value, String color, String nameOfFamily) {
         super(name, value, color, nameOfFamily);
--- src/main/java/fr/unice/polytech/citadelle/game/purple_districts/University.java
+++ src/main/java/fr/unice/polytech/citadelle/game/purple_districts/University.java
@@ -1,8 +1,8 @@
 package fr.unice.polytech.citadelle.game.purple_districts;
 
-import fr.unice.polytech.citadelle.game.District;
+import fr.unice.polytech.citadelle.game.BonusDistrict;
 
-public class University  extends District {
+public class University  extends BonusDistrict {
 
     public University(String name, int value, String color, String nameOfFamily) {
         super(name, value, color, nameOfFamily);
