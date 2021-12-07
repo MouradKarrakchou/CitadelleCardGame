c3f537c5c7fc59774ff4089cea9de5edc0c352a1
AyoubIMAMI <youbima17@gmail.com>
2021-12-07 09:18:59 +0100

    Adding district spell: Library Test 2 #31ù

--- src/test/java/fr/unice/polytech/citadelle/SpellDistrictTest.java
+++ src/test/java/fr/unice/polytech/citadelle/SpellDistrictTest.java
@@ -1,31 +1,30 @@
 package fr.unice.polytech.citadelle;
 
-import fr.unice.polytech.citadelle.game.Board;
-import fr.unice.polytech.citadelle.game.DeckCharacter;
 import fr.unice.polytech.citadelle.game.DeckDistrict;
 import fr.unice.polytech.citadelle.game.Player;
 import fr.unice.polytech.citadelle.game.purple_districts.Library;
 import fr.unice.polytech.citadelle.game_engine.Initialiser;
-import fr.unice.polytech.citadelle.game_engine.Referee;
 import fr.unice.polytech.citadelle.game_interactor.Executor;
 import fr.unice.polytech.citadelle.game_interactor.NormalBot;
+import fr.unice.polytech.citadelle.game_interactor.RushBot;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SpellDistrictTest {
 
     private ArrayList<Player> listOfPlayer = new ArrayList<>();
-    private DeckDistrict deckDistrict = new DeckDistrict();
+    private DeckDistrict deckDistrict = new DeckDistrict(); //new spyDeckDistrict
     private Initialiser initialiser = new Initialiser();
 
     Player player = new Player("Player");
     Executor executor = new Executor(player);
     NormalBot normalBot = new NormalBot(player);
+    RushBot rushBot = new RushBot(player);
 
     @Test
     void librarySpellNormalBotTest() {
@@ -43,6 +42,44 @@ public class SpellDistrictTest {
         initialiser.initDeckDistrict(deckDistrict);
 
         normalBot.normalBehaviour(deckDistrict);
+        assertEquals(1, player.getDistrictCards().size());
+    }
+
+    @Test
+    void librarySpellRushBotNormalBehaviorTest() {
+        player.getDistrictCards().clear();
+        initialiser.initDeckDistrict(deckDistrict);
+        executor.buildDistrict(new Library("Library", 6,"Purple","Prestige"));
+
+        rushBot.normalBehaviour(deckDistrict);
+        assertEquals(2, player.getDistrictCards().size());
+    }
+
+    @Test
+    void noLibrarySpellRushBotNormalBehaviorTest() {
+        player.getDistrictCards().clear();
+        initialiser.initDeckDistrict(deckDistrict);
+
+        rushBot.normalBehaviour(deckDistrict);
+        assertEquals(1, player.getDistrictCards().size());
+    }
+
+    @Test
+    void librarySpellRushBotEndGameBehaviorTest() {
+        player.getDistrictCards().clear();
+        initialiser.initDeckDistrict(deckDistrict);
+        executor.buildDistrict(new Library("Library", 6,"Purple","Prestige"));
+
+        rushBot.endGameBehaviour(deckDistrict);
+        assertEquals(2, player.getDistrictCards().size());
+    }
+
+    @Test
+    void noLibrarySpellRushBotEndGameBehaviorTest() {
+        player.getDistrictCards().clear();
+        initialiser.initDeckDistrict(deckDistrict);
+
+        rushBot.endGameBehaviour(deckDistrict);
         assertEquals(1, player.getDistrictCards().size());
     }
 
