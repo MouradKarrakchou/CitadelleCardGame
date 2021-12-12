65340acdee6cf926b7bdbfd55f574ea06f89d57a
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2021-12-12 15:15:38 +0100

    Adding javadoc, simplifying methods on PhaseManager class #41

--- src/main/java/fr/unice/polytech/citadelle/game_engine/PhaseManager.java
+++ src/main/java/fr/unice/polytech/citadelle/game_engine/PhaseManager.java
@@ -12,34 +12,41 @@ public class PhaseManager {
 	public final static String MID_GAME_PHASE = "MID_GAME_PHASE";
 	public final static String END_GAME_PHASE = "END_GAME_PHASE";
 	public final static String LAST_TURN_PHASE = "LAST_TURN_PHASE";
-
 	
 	public PhaseManager() {}
-	
+
+	/**
+	 * Analyse the game phase according to PhaseManager class parameters.
+	 * @param listCityOfPlayers The list of players' city.
+	 * @return If the game is in the last round, close to be finished or in process.
+	 */
 	public String analyseGame(ArrayList<City> listCityOfPlayers) {		
 		if(isLastTurnPhase(listCityOfPlayers)) return LAST_TURN_PHASE;
 		if(isEndGamePhase(listCityOfPlayers)) return END_GAME_PHASE;
-		
 		return MID_GAME_PHASE;
 	}
-	
+
+	/**
+	 * Check if the game is in the last turn according to the number of districts in each players' city.
+	 * @param listCityOfPlayers The list of players' city.
+	 * @return true/false : the game is in the last turn.
+	 */
 	private boolean isLastTurnPhase(ArrayList<City> listCityOfPlayers) {
-		boolean isLastTurn = false;
-		
 		for(City c : listCityOfPlayers)
-			if(c.isComplete()) isLastTurn = true;
-		
-		return isLastTurn;
+			if(c.isComplete())
+				return true;
+		return false;
 	}
-	
+
+	/**
+	 * Check if the game is in the end game according to the number of districts in each players' city.
+	 * @param listCityOfPlayers The list of players' city.
+	 * @return true/false : it's the end game.
+	 */
 	private boolean isEndGamePhase(ArrayList<City> listCityOfPlayers) {
-		boolean isLastTurn = false;
-		
 		for(City c : listCityOfPlayers)
-			if(c.getSizeOfCity() >= 6) isLastTurn = true;
-		
-		return isLastTurn;
+			if(c.getSizeOfCity() >= 6)
+				return true;
+		return false;
 	}
-	
-	
 }
