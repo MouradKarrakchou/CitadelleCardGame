5f99943d92175b7727c9d2086f0710a31b712192
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2021-12-14 18:37:48 +0100

    remove loop for

--- src/main/java/fr/unice/polytech/citadelle/Main.java
+++ src/main/java/fr/unice/polytech/citadelle/Main.java
@@ -7,9 +7,7 @@ public class Main {
 	public static void main(String... args) {
 			Controller controller = new Controller();
 
-			for(int i = 0 ; i < 100 ; i++) {
 				controller.initGame();
 				controller.runGame();
-			}
 	}
 }
