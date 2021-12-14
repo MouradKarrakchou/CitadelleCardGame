8006a3024d80339c483b4f998a20f7f08bcf0e30
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2021-12-14 18:59:22 +0100

    add shuffle for the deckDistrict #37

--- src/main/java/fr/unice/polytech/citadelle/Main.java
+++ src/main/java/fr/unice/polytech/citadelle/Main.java
@@ -6,8 +6,7 @@ public class Main {
 
 	public static void main(String... args) {
 			Controller controller = new Controller();
-
-				controller.initGame();
-				controller.runGame();
+			controller.initGame();
+			controller.runGame();
 	}
 }
