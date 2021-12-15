b70806a027d51759ee437f24740553b1cfe0af63
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2021-12-15 14:26:37 +0100

    Adding javadoc in Architect class #41

--- src/main/java/fr/unice/polytech/citadelle/characters_class/Architect.java
+++ src/main/java/fr/unice/polytech/citadelle/characters_class/Architect.java
@@ -7,13 +7,17 @@ import fr.unice.polytech.citadelle.output.PrintCitadels;
 import java.util.LinkedHashMap;
 import java.util.Optional;
 
+/**
+ * The architect automatically draws two cards.
+ * In addition of the tour collection (cards or coins), the architect can build up to 3 districts in one round.
+ */
 public class Architect extends Character {
 	
     public Architect(){
         super("Architect", 7);
     }
-    @Override
 
+    @Override
     public void spellOfTurn(Behaviour bot, LinkedHashMap<Character, Optional<Behaviour>> hashOfCharacters, PrintCitadels printC){
         //Giving 2 Cards to the Player with Architect character
         printC.printArchitectSpell();
