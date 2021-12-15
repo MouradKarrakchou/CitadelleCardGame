303cee0e33e6adaf0f7a6c0c1c8cd74fa245e7e8
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2021-12-15 14:30:56 +0100

    Adding javadoc in Assassin class #41

--- src/main/java/fr/unice/polytech/citadelle/characters_class/Assassin.java
+++ src/main/java/fr/unice/polytech/citadelle/characters_class/Assassin.java
@@ -1,16 +1,17 @@
 package fr.unice.polytech.citadelle.characters_class;
 
 import fr.unice.polytech.citadelle.game.Character;
-import fr.unice.polytech.citadelle.game.Player;
 import fr.unice.polytech.citadelle.game_interactor.Behaviour;
 import fr.unice.polytech.citadelle.output.PrintCitadels;
 
 import java.util.LinkedHashMap;
 import java.util.Optional;
 
+/**
+ * Assassin can kill one character per round. The player having the character being killed will be not
+ * able to play the round.
+ */
 public class Assassin extends Character{
-	Player target;
-	
     public Assassin(){
         super("Assassin", 1);
     }
@@ -18,7 +19,8 @@ public class Assassin extends Character{
     @Override
     public void spellOfTurn(Behaviour bot, LinkedHashMap<Character, Optional<Behaviour>> hashOfCharacters, PrintCitadels printC){
         Character characterToDie= bot.selectCharacterForSpell(hashOfCharacters);
-        if (hashOfCharacters.get(characterToDie).isPresent()) hashOfCharacters.get(characterToDie).get().setCharacterIsAlive(false);
+        if (hashOfCharacters.get(characterToDie).isPresent())
+            hashOfCharacters.get(characterToDie).get().setCharacterIsAlive(false);
         printC.killCharacter(characterToDie);
     }
 }
