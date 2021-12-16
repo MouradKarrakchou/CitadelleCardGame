86a6df3d3dbeb2cd1a1a4e481e925bfee34e3b9c
AyoubIMAMI <youbima17@gmail.com>
2021-12-16 16:52:54 +0100

    Predict the characters that will be chosen by other players #51

--- src/main/java/fr/unice/polytech/citadelle/game_interactor/Predict.java
+++ src/main/java/fr/unice/polytech/citadelle/game_interactor/Predict.java
@@ -2,6 +2,7 @@ package fr.unice.polytech.citadelle.game_interactor;
 
 import fr.unice.polytech.citadelle.characters_class.Assassin;
 import fr.unice.polytech.citadelle.game.Character;
+import fr.unice.polytech.citadelle.game.District;
 import fr.unice.polytech.citadelle.game.Player;
 
 import java.util.ArrayList;
@@ -9,8 +10,33 @@ import java.util.ArrayList;
 public class Predict {
     public Predict(){
     }
-    Character predictWhoIsPlayer(Player player, ArrayList<String> listOfCharacterToNotDoTheSpellOn){
-        //A changer il faut une methode qui predisent ce que le player a.
+    Character predictWhoIsPlayer(Player player, ArrayList<String> listOfUntargetableCharacter){
+        //A changer : il faut une methode qui predit ce que le player a.
+        ArrayList<String> listOfTargetableCharacter = allCharacters();
+
+        for(String character : listOfUntargetableCharacter) {
+
+        }
+
+        int goldsOfPlayer = player.getGolds();
+        ArrayList<District> handOfPlayer = player.getDistrictCards();
+        ArrayList<District> cityOfPlayer = player.getCity().getBuiltDistrict();
+
+
         return (new Assassin());
+    }
+
+    ArrayList<String> allCharacters() {
+        ArrayList<String> listOfTargetableCharacter = new ArrayList<>();
+        listOfTargetableCharacter.add("Architect");
+        listOfTargetableCharacter.add("Assassin");
+        listOfTargetableCharacter.add("Bishop");
+        listOfTargetableCharacter.add("King");
+        listOfTargetableCharacter.add("Magician");
+        listOfTargetableCharacter.add("Merchant");
+        listOfTargetableCharacter.add("Thief");
+        listOfTargetableCharacter.add("Warlord");
+
+        return listOfTargetableCharacter;
     }
 }
