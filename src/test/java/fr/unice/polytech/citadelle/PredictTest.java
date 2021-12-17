619cc98cbdb9677e1b5c25cbb02fde2ef3499dd8
AyoubIMAMI <youbima17@gmail.com>
2021-12-17 23:22:00 +0100

    Predict the characters that will be chosen by other players : tests #51

--- src/test/java/fr/unice/polytech/citadelle/PredictTest.java
+++ src/test/java/fr/unice/polytech/citadelle/PredictTest.java
@@ -1,7 +1,7 @@
 package fr.unice.polytech.citadelle;
 
 import fr.unice.polytech.citadelle.characters_class.Thief;
-import fr.unice.polytech.citadelle.game.Board;
+import fr.unice.polytech.citadelle.game.*;
 import fr.unice.polytech.citadelle.game.Character;
 import fr.unice.polytech.citadelle.game_engine.Initializer;
 import fr.unice.polytech.citadelle.game_interactor.Predict;
@@ -15,13 +15,16 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class PredictTest {
 
+    //Board board = new Board(new ArrayList<Player>(),new ArrayList<Character>(), new DeckDistrict(), new DeckCharacter());
     Board board = new Board();
+    Initializer initializer = new Initializer();
     Predict predict = new Predict(board);
-
     ArrayList<Character> allCharacters = board.getListOfCharacter();
 
     @Test
     void targetableCharactersForPredictWhoIsPlayerTest() {
+
+        initializer.createListOfAllCharacter();
         ArrayList<Character> untargetableCharacter = new ArrayList<>();
         untargetableCharacter.add(board.getListOfCharacter().get(Initializer.BISHOP_INDEX));
         untargetableCharacter.add(board.getListOfCharacter().get(Initializer.THIEF_INDEX));
