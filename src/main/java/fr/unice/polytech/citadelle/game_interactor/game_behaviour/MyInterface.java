ce32e6365b38d1c514d00a519fdc7048002994e5
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-01-04 13:15:08 +0100

    create hasmap of strategy template #69

---
+++ src/main/java/fr/unice/polytech/citadelle/game_interactor/game_behaviour/MyInterface.java
@@ -0,0 +1,7 @@
+package fr.unice.polytech.citadelle.game_interactor.game_behaviour;
+import fr.unice.polytech.citadelle.game_character.Character;
+
+
+public interface MyInterface {
+	Character doSomething(Character character, String value);
+}
