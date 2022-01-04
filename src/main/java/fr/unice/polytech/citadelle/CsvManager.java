cb0507a13aa5293563d638e73586fbda0aeb2d29
AyoubIMAMI <youbima17@gmail.com>
2022-01-04 17:04:44 +0100

    CSV file : saving the winner of the game #65

--- src/main/java/fr/unice/polytech/citadelle/CsvManager.java
+++ src/main/java/fr/unice/polytech/citadelle/CsvManager.java
@@ -9,24 +9,30 @@ import fr.unice.polytech.citadelle.game_engine.Referee;
 
 public class CsvManager {
 
+
     ArrayList<Player> leaderboard = new ArrayList<>();
 
     CsvManager (ArrayList<Player> leaderboard) {
         this.leaderboard = leaderboard;
     }
 
-    String winner() {
-        return leaderboard.get(0).getName();
-    }
 
-    void write(String[] args) throws Exception {
+
+    void write() throws Exception {
         String csv = "results.csv";
         CSVWriter writer = new CSVWriter(new FileWriter(csv));
 
         //Create record
-        String [] record = "4,David,Miller,Australia,30".split(",");
+        String [] record = "BotType,1,2,3,4,5,6,7,8,Win rate,Games played".split(",");
         //Write the record to file
         writer.writeNext(record);
+
+        String toWrite = "";
+        int numberOfPlayers = leaderboard.size();
+        for(int i = 0; i < numberOfPlayers; i++) {
+            toWrite += String.valueOf(i + 1);
+        }
+        
 
         //close the writer
         writer.close();
