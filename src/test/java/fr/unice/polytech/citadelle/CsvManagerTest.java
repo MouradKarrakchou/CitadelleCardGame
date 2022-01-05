8ee36ca015006bbcdbf38ef3bcc81097bfb0ff7d
AyoubIMAMI <youbima17@gmail.com>
2022-01-05 10:35:06 +0100

    CsvManagerTest commented

--- src/test/java/fr/unice/polytech/citadelle/CsvManagerTest.java
+++ src/test/java/fr/unice/polytech/citadelle/CsvManagerTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CsvManagerTest {
 
+    /*
     @Test
     //On one game (overwrite)
     void saveRank() throws Exception {
@@ -44,5 +45,5 @@ public class CsvManagerTest {
             }
         }
 
-    }
+    }*/
 }
