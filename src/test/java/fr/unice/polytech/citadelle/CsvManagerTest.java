aca832f75d3d3eea2eae7891867919e133ed0a74
AyoubIMAMI <youbima17@gmail.com>
2022-01-05 20:30:52 +0100

    Csv createFile test #60 #63

--- src/test/java/fr/unice/polytech/citadelle/CsvManagerTest.java
+++ src/test/java/fr/unice/polytech/citadelle/CsvManagerTest.java
@@ -36,52 +36,12 @@ public class CsvManagerTest {
     }
 
     @Test
-    //On one game (overwrite)
     void createFile() throws Exception {
-
-        String [] fields = {"BotType", "1", "2", "3", "4", "5", "6", "7", "Win-rate", "Games played", "Total score", "Average score"};
         csvManager.createFile();
-        CSVReader reader = new CSVReader(new FileReader("save\\results.csv"), ',', '"', 0);
-
-        String a = fields[1];
-        String b = reader.readAll().get(0)[1];
-        assertEquals(a, b);
-
-        a = fields[1];
-        b = reader.readAll().get(0)[1];
-        assertEquals(a, b);
-
-//        for(int i = 0; i < 12; i++) {
-//            assertEquals(fields[i], reader.readAll().get(0));
-//        }
-
-        /*
-        ArrayList<Player> leaderboard = new ArrayList<>();
-        leaderboard.add(new Player("Player2"));
-        leaderboard.add(new Player("Player4"));
-        leaderboard.add(new Player("Player1"));
-        leaderboard.add(new Player("Player3"));
-
-        String [] realLeaderboard = {
-                "[BotType, 1, 2, 3, 4, 5, 6, 7, Win-rate, Games played]",
-                "[Player2, 1, 0, 0, 0, 0, 0, 0, 100.0, 1]",
-                "[Player4, 0, 1, 0, 0, 0, 0, 0, 0.0, 1]",
-                "[Player1, 0, 0, 1, 0, 0, 0, 0, 0.0, 1]",
-                "[Player3, 0, 0, 0, 1, 0, 0, 0, 0.0, 1]"
-        };
-
-        CsvManager csvManager = new CsvManager(leaderboard);
-        csvManager.append();
-
-        CSVReader reader = new CSVReader(new FileReader("src\\main\\resources\\save\\results.csv"), ',' , '"' , 0);
-        int counter = 0;
-        String[] nextLine;
-        while ((nextLine = reader.readNext()) != null) {
-            if (nextLine != null) {
-                assertEquals(realLeaderboard[counter],Arrays.toString(nextLine));
-                counter++;
-            }
+        for(int i = 0; i < 12; i++) {
+            CSVReader readCreatedFile = new CSVReader(new FileReader("save\\results.csv"), ',', '"', 0);
+            CSVReader readFileTest = new CSVReader(new FileReader("csvTests\\createFile.csv"), ',', '"', 0);
+            assertEquals(readFileTest.readAll().get(0)[i], readCreatedFile.readAll().get(0)[i]);
         }
-*/
     }
 }
