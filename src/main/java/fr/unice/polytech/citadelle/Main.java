737e198b88294c4817d7cc5bb3c14e8bbcda1425
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-01-06 12:32:43 +0100

    Hiding logs, setting up one bot of each category for the game

--- src/main/java/fr/unice/polytech/citadelle/Main.java
+++ src/main/java/fr/unice/polytech/citadelle/Main.java
@@ -12,14 +12,14 @@ import fr.unice.polytech.citadelle.output.PrintCitadels;
  * @author BONNET Kilian, IMAMI Ayoub, KARRAKCHOU Mourad, LE BIHAN Léo
  */
 public class Main {
-	private static final int NUMBER_OF_RICHALPHONSE = 3;
-	private static final int NUMBER_OF_INVESTOR =  0;
-	private static final int NUMBER_OF_RUSHER = 0;
+	private static final int NUMBER_OF_RICHALPHONSE = 1;
+	private static final int NUMBER_OF_INVESTOR =  1;
+	private static final int NUMBER_OF_RUSHER = 1;
 	private static final int NUMBER_OF_STRATEGATOR = 1;
 
 	public static void main(String... args) throws Exception {
 		// Running a game
-		PrintCitadels.activateLevelInfo();
+		PrintCitadels.activateLevelWarning();
 		Controller controller = new Controller();
 		controller.initGame(NUMBER_OF_RICHALPHONSE, NUMBER_OF_INVESTOR, NUMBER_OF_RUSHER, NUMBER_OF_STRATEGATOR);
 
